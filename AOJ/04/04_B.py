7ef2826d0e608f3cd017ef8774893c8109355342
tikutikuwa <otsuka63477@gmail.com>
2022-10-06 15:21:08 +0900

    submit_AOJ04/#1

---
+++ AOJ/04/04_B.py
@@ -0,0 +1,6 @@
+r = float(input())
+pai = 3.141592653589
+
+print("{0:.6f} {1:.6f}".format(pai*r**2,2*pai*r))
+
+
---
+++ AOJ/04/04_D.py
@@ -0,0 +1,3 @@
+n = int(input())
+value = list(map(int,input().split()))
+print(min(value),max(value),sum(value))
