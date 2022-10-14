a7978b371836326c7d8ffbef05fbe723069f67bc
tikutikuwa <otsuka63477@gmail.com>
2022-10-15 00:04:41 +0900

    submit_AOJ08/#1

---
+++ AOJ/08/08_C.py
@@ -0,0 +1,13 @@
+count_ls = [0 for i in range(26)]
+
+while(True):
+    try:
+        str = input().lower()
+        for ch in str:
+            if (ord("a") <= ord(ch) <= ord("z")):
+                count_ls[ord(ch)-ord("a")] += 1
+    except EOFError:
+        break
+
+for i in range(len(count_ls)):
+    print("{0} : {1}".format(chr(i+ord("a")),count_ls[i]))
